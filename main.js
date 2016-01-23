b707acb0482ab4d0bf4fda244f4a44ba85416d9c
Kate Wilson <wilsonkate.kw@gmail.com>
2016-01-22 23:22:54 -0500

    video portfolio site

---
+++ main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+});
+
+// $(document).ready(function() {
+//     $('#pagepiling').pagepiling();
+// });
+
+// $('#pagepiling').pagepiling({
+//     sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+// });
+//
+// $('#pagepiling').pagepiling({
+//     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+//     menu: '#myMenu'
+// });

70fc377886c8d5ae447bf41c04a788652befc3b4
Kate Wilson <wilsonkate.kw@gmail.com>
2016-01-23 18:46:15 -0500

    mermaid

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
-$(document).ready(function() {
-    $('#fullpage').fullpage();
-});
+// 
+// $(document).ready(function() {
+//     $('#fullpage').fullPage();
+// });
 
 // $(document).ready(function() {
 //     $('#pagepiling').pagepiling();
@@ -15,3 +15,58 @@ $(document).ready(function() {
 //     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
 //     menu: '#myMenu'
 // });
+
+
+// $.fn.fullpage({
+//     scrollOverflow: false
+// });
+
+
+  //   function iedetect(v) {
+	//     var r = RegExp('msie' + (!isNaN(v) ? ('\\s' + v) : ''), 'i');
+	// 	return r.test(navigator.userAgent);
+  //
+	// }
+
+  //
+  // $.fn.fullpage({
+  //     scrollOverflow: false,
+  //     afterRender: function(){
+  //         $('#video')[0].play();
+  //     }
+  // });
+
+		// Wait until the video meta data has loaded
+		// $('#section0 video').on('loadedmetadata', function() {
+    //
+		// 	var $width, $height, // Width and height of screen
+		// 		$vidwidth = this.videoWidth, // Width of video (actual width)
+		// 		$vidheight = this.videoHeight, // Height of video (actual height)
+		// 		$aspectRatio = $vidwidth / $vidheight; // The ratio the video's height and width are in
+    //
+		// 	(adjSize = function() { // Create function called adjSize
+    //
+		// 		$width = $(window).width(); // Width of the screen
+		// 		$height = $(window).height(); // Height of the screen
+    //
+		// 		$boxRatio = $width / $height; // The ratio the screen is in
+    //
+		// 		$adjRatio = $aspectRatio / $boxRatio; // The ratio of the video divided by the screen size
+    //
+		// 		// Set the container to be the width and height of the screen
+		// 		$('#section0').css({'width' : $width+'px', 'height' : $height+'px'});
+    //
+		// 		if($boxRatio < $aspectRatio) { // If the screen ratio is less than the aspect ratio..
+		// 			// Set the width of the video to the screen size multiplied by $adjRatio
+		// 			$vid = $('#section0 video').css({'width' : $width*$adjRatio+'px'});
+		// 		} else {
+		// 			// Else just set the video to the width of the screen/container
+		// 			$vid = $('#section0 video').css({'width' : $width+'px'});
+		// 		}
+    //
+		// 	})(); // Run function immediately
+    //
+		// 	// Run function also on window resize.
+		// 	$(window).resize(adjSize);
+		// 	$('#video')[0].play();
+		// });
